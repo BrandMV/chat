78b572eece3b5bce78b5e9b093f6367b427c6d9d
BrandMV <brandonmv2001@gmail.com>
2021-05-10 18:39:26 -0500

    Bugs fixed

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,15 +2,15 @@ import firebase from "firebase/app"
 import "firebase/auth";
 import "firebase/firestore"
 
-const {REACT_APP_APIKEY, REACT_APP_AUTHDOMAIN, REACT_APP_PROJECTID, REACT_APP_STORAGEBUCKET, REACT_APP_MESSAGINGSENDERID, REACT_APP_APPID} = process.env
+// const {REACT_APP_APIKEY, REACT_APP_AUTHDOMAIN, REACT_APP_PROJECTID, REACT_APP_STORAGEBUCKET, REACT_APP_MESSAGINGSENDERID, REACT_APP_APPID} = process.env
 
 firebase.initializeApp({
-    apiKey: REACT_APP_APIKEY,
-    authDomain: REACT_APP_AUTHDOMAIN,
-    projectId: REACT_APP_PROJECTID,
-    storageBucket: REACT_APP_STORAGEBUCKET,
-    messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-    appId: REACT_APP_APPID,
+    apiKey: "AIzaSyBQMmlZidQl8gZDCCTWOrj6ko6WrT5Nx-Y",
+    authDomain: "react-chat-ts-97131.firebaseapp.com",
+    projectId: "react-chat-ts-97131",
+    storageBucket: "react-chat-ts-97131.appspot.com",
+    messagingSenderId: "135441841794",
+    appId: "1:135441841794:web:f05eeb2bf45f9819617e9b"
 })
 
 const auth = firebase.auth();
